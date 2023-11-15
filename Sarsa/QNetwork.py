5a3b89284d091e47dc741703348a15bf8d7b76dc
GeremyAndOrange <605200361@qq.com>
2023-11-15 12:20:00 +0800

    modified:   Sarsa/QNetwork.py 	new file:   Sarsa/__pycache__/QNetwork.cpython-311.pyc

--- Sarsa/QNetwork.py
+++ Sarsa/QNetwork.py
@@ -1,17 +1,15 @@
 import torch
 
 class QNetwork(torch.nn.Module):
-    def __init__(self, in_dim) -> None:
+    def __init__(self, in_dim,out_dim) -> None:
         super(QNetwork,self).__init__()
         layers = [
             torch.nn.Linear(in_dim,64),
             torch.nn.ReLU(),
-            torch.nn.Linear(64,1),
+            torch.nn.Linear(64,out_dim),
             ]
         self.model = torch.nn.Sequential(*layers)
+        self.train()
     
-    def forward(self,action,state):
-        state = torch.tensor([state]).float().view(1, 1)
-        action = torch.tensor([action]).float().view(1, 1)
-        input = torch.cat((action, state), dim=1)
-        return self.model(input)
+    def forward(self,x):
+        return self.model(x)
